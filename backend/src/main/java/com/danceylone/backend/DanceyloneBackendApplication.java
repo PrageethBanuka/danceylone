040908a75557485914ce2490d2ecebadddc06082
PrageethBanuka <prageethbanukarajapaksha@gmail.com>
2025-12-21 18:30:56 +0530

    Initial Spring Boot project for Danceylone backend

---
+++ backend/src/main/java/com/danceylone/backend/DanceyloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.danceylone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceyloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceyloneBackendApplication.class, args);
+	}
+
+}
